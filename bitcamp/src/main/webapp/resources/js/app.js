a75a35c0f97504d5384b4a21eaee4627cc0eeae6
kimdooyoung4862 <skyfol1005@naver.com>
2018-03-19 17:36:06 +0900

    trans

--- bitcamp/src/main/webapp/resources/js/app.js
+++ bitcamp/src/main/webapp/resources/js/app.js
@@ -1,12 +1,11 @@
 var app = app || {};
-var router = router || {};
-app = (()=>{
+/*app = (()=>{
 	var init=x=>{
 		onCreate(x);
 		setContentView();
 	};
 	var onCreate=x=>{
-		route.init(x);
+		app.router.onCreate(x);
 	};
 	var setContentView=()=>{
 		$('#wrapper').empty();
@@ -14,33 +13,49 @@ app = (()=>{
 	};
 	return {init:init};
 })();
+*/
 
-app.algorithm = (()=>{
-	var init=()=>{
-		onCreate();
-		setContentView();
+app = (()=>{
+	var init=x=>{
+		$.getScript(x+'/resources/js/router.js',()=>{
+			$.extend(new Router(x));
+			app.algorithm.onCreate();
+		})
 	};
-	var onCreate=()=>{
-		
-	};
-	var setContentView=()=>{
-		$('#wrapper').html('<h1>Hello AJAX !!</h1>');
-	};
-	return{init:init};
+	return {init:init};
 })();
 
-router = (()=>{
-	var init=x=>{
-		sessionStorage.setItem('x',x);
-	};
+app.algorithm = (()=>{
+	var $wrapper,context,algorithm,view;
 	var onCreate=()=>{
-		
+		$wrapper = $('#wrapper');
+		context = $.context();
+		algorithm = $.javascript()+'/algorithm.js';
+		view = $.javascript()+'/view.js';
+		setContentView();
 	};
 	var setContentView=()=>{
-		
+		$wrapper.empty();
+		$.getScript(view,()=>{
+			$wrapper.html(navigation(image));
+			var button = '<button id="btn-nav-1st" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">'
+			    +'        <span class="sr-only">Toggle navigation</span>'
+			    +'        <span class="icon-bar"></span>'
+			    +'        <span class="icon-bar"></span>'
+			    +'        <span class="icon-bar"></span>'
+			    +'      </button>';
+			$('#div-nav-1st').append(button);
+			$('#btn-nav-1st').click(()=>{
+				alert('Button Click !!');
+			});
+			var aLogin =  ' <a id="a-login" href="#"> '
+		    +'              <span class="glyphicon glyphicon-user" aria-hidden="true">&nbsp;로그인</span>'
+		    +'              </a>';
+			$('#li-login').append(aLogin);
+			$('#a-login').click(()=>{
+				alert('LOGIN BUTTON CLICK');
+			});
+		});
 	};
-	var $ = () => {
-		return sessionStorage.getItem('x');
-	};
-	return {init:init,$:$};
+	return{onCreate:onCreate};
 })();
